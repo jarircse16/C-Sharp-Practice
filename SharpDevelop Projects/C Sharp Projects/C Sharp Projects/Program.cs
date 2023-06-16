3a9af805ee8d1ddeda195cdf8183dd045c826f75
Jarir Ahmed Deedat <129315010+jarircse16@users.noreply.github.com>
2023-06-16 20:32:06 +0600

    C-Sharp-Practice
    C-Sharp-Practice

---
+++ SharpDevelop Projects/C Sharp Projects/C Sharp Projects/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace C_Sharp_Projects
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("I am Jarir and This is my "); //No new lines
+			Console.WriteLine("1st Program in C#");
+			Console.WriteLine(500); //Message with new line
+			
+			// TODO: Implement Functionality Here
+			
+			//string declaration
+    		string message = "You are the best";
+   			Console.WriteLine(message);
+   			
+   			int points = 44;
+   			Console.WriteLine(points);
+   			
+   			double weight = 95.4;
+   			Console.WriteLine(weight);
+   			
+   			float height = 1.82f;
+   			Console.WriteLine(height);
+   			
+   			decimal weight1 = 92.8m;
+   			Console.WriteLine(weight1);
+   			
+   			bool x= true;
+   			bool y= false;
+   			Console.WriteLine(x || y);
+   			Console.WriteLine(5+4-3);
+   			int xx = 5;
+   			int yy = 4;
+   			int zz = 3;
+   			Console.WriteLine(xx+yy-zz);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+			
+			
+		}
+	}
+}
---
+++ SharpDevelop Projects/C Sharp Projects/Project 2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ASUS
+ * Date: 4/30/2023
+ * Time: 11:55 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Project_2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
